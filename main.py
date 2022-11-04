5d3d1369ed7082aa02b7dc0e2cd2245ddf46aa37
fulanii <yassineissoufou21@gmail.com>
2022-11-04 17:43:30 -0500

    added pro section

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 
-
-# Small scrip to creat readme files for each day in the beginner section
 import os
 
+# Small scrip to creat readme files for each day in the beginner section
 os.chdir(f"BeginnerSection")
 
 for i in range(1, 15):
@@ -16,5 +15,19 @@ for i in range(1, 15):
         with open("README.md", "x") as file:
             file.write(f"##Day {i}")
         os.chdir("..") 
+print("Done")
+
+
+# Small scrip to creat readme files for each day in the Intermediate section
+os.chdir(f"IntermediateSection")
+
+for i in range(15, 59):
+    day = os.mkdir(f"day{i}")
+    os.chdir(f"day{i}")
+
+    with open("README.md", "x") as file:
+        file.write(f"# Day {i} - ")
+    os.chdir("..")
+ 
 print("Done")
 
